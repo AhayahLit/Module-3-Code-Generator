5b53c32bdca92883e3692654f2ae80fff0a9933b
AhayahLit <lau1087054@gmail.com>
2022-07-17 18:24:39 -0700

    letter and symbols prompt

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
 var generateBtn = document.querySelector("#generate");
+let Numbers= ["1","2","3","4","5","6","7","8","9","0"];
+let Symbols= ["!@#$%^&*()"];
+let LowerCase= ["abcdefghijklmnopqrstuvwxyz"];
+let UpperCase= ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
 function generatePassword() {
     var passwordLength= prompt("What is the length of the password?");
 console.log(passwordLength);
 if (passwordLength < 8 || passwordLength > 128) {
 alert("Invalid Password count");
 return;
-} 
+    }
+}
+function prompt(question) {
+    let response= prompt(question).tolowercase();
+    do {
+        if (response === "y" || "yes") {
+            return true;
+        }
+        else if (response === "n" || "no") {
+            return false;
+        }
+        else {
+            response= prompt("Y/N or Yes or No is not specified" + question).tolowercase();  
+        }
+    } while (true);
+}
+     
+do {
+    useNumbers= prompt("Would you like to use numbers in your password? (y or n or yes or no)");
+    useLowerCase= prompt("Would you like to use lower case letters? (y or n or yes or no)");
+    useUpperCase= prompt("Would you like to use upper case letters? (y or n or yes or no)");
+    useSymbols= prompt("Would you like to use symbols? (y or n or yes or no)");
+    if(!Numbers && !useLowerCase && !UpperCase && !Symbols);
+    alert("Please choose a method to generate your password");
+}
 
-
-}     
+let passArray= [];
+    if (useNumbers) {
+        passArray.splice(0,0,...Numbers);
+    } 
+    if (useLowerCase) {
+        passArray.splice(0,0...LowerCase);
+    }
+    if (useUpperCase) {
+        passArray.splice(0,0...UpperCase);
+    }
+    if (useSymbols) {
+        passArray.splice(0,0...Symbols);
+    }
 
 function writePassword() {
   var password = generatePassword()
   var passwordText = document.querySelector("#password");
-    
-    // charset= ["1-0", "!-)", "a-z", "A-Z"];
-    // for (i=0, n=charset.length; i<length; i++) {
-    //     retVal += charAT(Math.floor(Math.random()*n));
-    // };
-  passwordText.value = password;
+   passwordText.value= " "   
+  
+     passwordText.value = password;
 }
 generateBtn.addEventListener("click", writePassword);
 
